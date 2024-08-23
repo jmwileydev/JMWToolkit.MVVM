5a78d3f85c087baa1df36e3286cb3a9b08a9e923
jmwileycms <jmwiley@cellmicrosystems.com>
2024-08-23 16:09:56 -0400

    - Add the basic rounded corner button. - Add a new brushes file and all of the colors for the default Rounded Corner button.

---
+++ Controls/RoundedCornerButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace JMWToolkit.MVVM.Controls;
+
+/// <summary>
+/// This is a button which allows you to specify a rounded corner.
+/// </summary>
+public class RoundedCornerButton : Button
+{
+    /// <summary>
+    /// The CornerRadius for the button. Default is (0,0,0,0)
+    /// </summary>
+    public CornerRadius CornerRadius
+    {
+        get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+        set { SetValue(CornerRadiusProperty, value); }
+    }
+
+    /// <summary>
+    /// The DependencyProperty for the CornerRadius
+    /// </summary>
+    public static readonly DependencyProperty CornerRadiusProperty =
+        DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(RoundedCornerButton), new PropertyMetadata(new CornerRadius(0, 0, 0, 0)));
+}
